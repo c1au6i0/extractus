c82e167510d5a667c57328eaa471a60231eca458
claudio.zanettini <claudio.zanettini@gmail.com>
2024-06-04 13:55:21 -0400

    feat: cols

--- data-raw/other.R
+++ data-raw/other.R
@@ -34,8 +34,6 @@ results <- dat |>
 unique(dplyr::bind_rows(results$endPoints)$assay)
 
 
-d
-
 library(rvest)
 html <- read_html("https://cfpub.epa.gov/ncea/iris/search/basic/index.cfm?first_letter=&keyword=&roe=oral&roe=inhalation&oral_tox_ncvminb=&oral_tox_ncvmine=%0D%0A%09%09%090&oral_tox_ncvmaxb=&oral_tox_ncvmaxe=%0D%0A%09%09%090&inh_tox_ncvminb=&inh_tox_ncvmine=%0D%0A%09%09%090&inh_tox_ncvmaxb=&inh_tox_ncvmaxe=%0D%0A%09%09%090&oral_sp_inter_rng=%0D%0A%09all&oral_sp_intra_rng=%0D%0A%09all&oral_sp_subc_rng=%0D%0A%09all&oral_sp_loael_rng=%0D%0A%09all&oral_sp_db_incomp_rng=%0D%0A%09all&oral_sp_other_rng=%0D%0A%09all&inhal_sp_inter_rng=%0D%0A%09all&inhal_sp_intra_rng=%0D%0A%09all&inhal_sp_subc_rng=%0D%0A%09all&inhal_sp_loael_rng=%0D%0A%09all&inhal_sp_db_incomp_rng=%0D%0A%09all&inhal_sp_other_rng=%0D%0A%09all&oral_sv_c_minb=&oral_sv_c_mine=%0D%0A%09%090&oral_sv_c_maxb=&oral_sv_c_maxe=%0D%0A%09%090&inh_tox_c_v_minb=&inh_tox_c_v_mine=%0D%0A%09%090&inh_tox_c_v_maxb=&inh_tox_c_v_maxe=%0D%0A%09%090/")
 html %>%
